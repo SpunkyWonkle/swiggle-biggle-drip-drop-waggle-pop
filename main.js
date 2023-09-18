ca87ce501567e22068588837be1d2b99576772c4
SpunkyWonkle <118027686+SpunkyWonkle@users.noreply.github.com>
2023-09-18 17:57:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+startClassification(microphone.access, ml5.soundClassifier(modelReady(classifieclassify(gotResults(gotResults(if error = yes then print.error else var red= 1 var blue = 176 var green= 247))))))
+<header if sound = cat then set cat to one and set sound to cat set color to random between red green and blue></header>
+var dog
+var img = ear-clipart-small-3.png
+var cat
+var rabbit
+var donkey
